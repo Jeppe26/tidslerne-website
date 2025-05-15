b0d062610f941c456d4c0c78279e91cd111cc5b8
AndersJ2812 <andjoe02@easv365.dk>
2025-05-15 09:41:36 +0200

    CSS ret
    Nyt CSS

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <?php $Footer = get_field("footer"); ?>
+    <p>&copy; <?php echo $Footer; ?></p>
+</footer>
+</main>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tidslerne</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        Slet hvis det virker... 
+        <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> */ --> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php bloginfo("name"); ?></title>
+        <?php wp_head(); ?>
+    </head>
