023733535849be4bdd81f6f0ecfc3925ad34ea8f
benjasteck <benj5102@easv365.dk>
2025-05-15 11:43:07 +0200

    test

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 <?php if (have_posts()): ?>
 <?php while (have_posts()): the_post(); ?>
-
+<?php
+  $logoImage = get_field("logo");
+?>
+<img src="<?php echo esc_url($logoImage["url"]); ?>" alt="logo">
 <?php endwhile; ?>
 <?php endif; ?>
