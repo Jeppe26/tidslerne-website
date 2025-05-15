8b90cf14e8a582c429235c8749a8e6279bb965b5
AndersJ2812 <andjoe02@easv365.dk>
2025-05-15 09:45:55 +0200

    CSS
    nyyyyyt

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
 function bst_load_resources() {
-    wp_enqueue_style("main-css", get_template_directory_uri() . "style.css");
+    wp_enqueue_style("main-css", get_template_directory_uri() . "/style.css");
 }
 add_action("wp_enqueue_scripts", "bst_load_resources");
