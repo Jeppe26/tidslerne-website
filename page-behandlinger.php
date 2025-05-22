77c004e58c3b533108b1859e613af1ef2346f92d
AndersJ2812 <andjoe02@easv365.dk>
2025-05-22 11:27:23 +0200

    Update page-behandlinger.php

--- page-behandlinger.php
+++ page-behandlinger.php
@@ -49,89 +49,11 @@ $ArtikelTitel = get_field("artikel_titel");
     <p>Kræftforeningen Tidslerne er en forening for tidligere og nuværende kræftpatienter samt deres pårørende og i øvrigt enhver, der ønsker at støtte.</p>
 </div>
 <div class="behandlingsKategorier">
-    <div class="behandlingsKat">
-        <div class="behandlingsKatLeft">
-            <img src="images/piller.jpg" alt="">
-        </div>
-        <div class="behandlingsKatRight">
-            <div class="scrollProgress">
-                <div class="scrollProgresContent">
-
-                </div>
-            </div>
-            <h3>Konventionel</h3>
-            <p>Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</p>
-            <button>LÆS MERE</button>
-        </div>
-    </div>
 
 
-    <div class="behandlingsKat">
-        <div class="behandlingsKatLeft">
-            <img src="images/frisør.jpg" alt="">
-        </div>
-        <div class="behandlingsKatRight">
-            <div class="scrollProgress">
-                <div class="scrollProgresContent">
-
-                </div>
-            </div>
-            <h3>Komplimentær</h3>
-            <p>Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</p>
-            <button>LÆS MERE</button>
-        </div>
-    </div>
 
 
-    <div class="behandlingsKat">
-        <div class="behandlingsKatLeft">
-            <img src="images/cannabis.jpg" alt="">
-        </div>
-        <div class="behandlingsKatRight">
-            <div class="scrollProgress">
-                <div class="scrollProgresContent">
-
-                </div>
-            </div>
-            <h3>Cannabis</h3>
-            <p>Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</p>
-            <button>LÆS MERE</button>
-        </div>
-    </div>
-
-
-    <div class="behandlingsKat">
-        <div class="behandlingsKatLeft">
-            <img src="images/hvorDørRebet.jpg" alt="">
-        </div>
-        <div class="behandlingsKatRight">
-            <div class="scrollProgress">
-                <div class="scrollProgresContent">
-
-                </div>
-            </div>
-            <h3>Behandling i udlandet</h3>
-            <p>Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</p>
-            <button>LÆS MERE</button>
-        </div>
-    </div>
-
-
-    <div class="behandlingsKat">
-        <div class="behandlingsKatLeft">
-            <img src="images/aura.jpg" alt="">
-        </div>
-        <div class="behandlingsKatRight">
-            <div class="scrollProgress">
-                <div class="scrollProgresContent">
-
-                </div>
-            </div>
-            <h3>Følge og senskader</h3>
-            <p>Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</p>
-            <button>LÆS MERE</button>
-        </div>
-    </div>
+        
 </div>
 </section>
 
