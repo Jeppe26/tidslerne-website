f61895e3a4df1c966653ca84e9f73a5a5194719c
AndersJ2812 <andjoe02@easv365.dk>
2025-05-22 12:15:06 +0200

    Update page-event-side.php

--- page-event-side.php
+++ page-event-side.php
@@ -38,59 +38,17 @@
         
       </section>-->
       <section class="eventList">
-      <div style="width: calc(100%-60px); height: 500px; position: relative;" class="svgBowling">
-        <svg
-          viewBox="0 0 1837 500"
-          width="calc(100%-60px)"
-          height="100%"
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style="display: block; width: 100%; "
-        >
-          <mask id="clip-mask" maskUnits="objectBoundingBox">
-            <path fill="white" d="M0 50C0 22.3858 22.3858 0 50 0H1787C1814.61 0 1837 22.3858 1837 50V129.5C1837 157.114 1814.61 179.5 1787 179.5H1507.25C1482.81 179.5 1463 199.311 1463 223.75C1463 248.189 1443.19 268 1418.75 268H1078.5C1050.89 268 1028.5 290.386 1028.5 318V450C1028.5 477.614 1006.11 500 978.5 500H50C22.3858 500 0 477.614 0 450V50Z"/>
-          </mask>
-      
-          <image class="shapeImage"
-            href="images/bowling2.png"
-            width="1837"
-            height="500"
-            mask="url(#clip-mask)"
-            preserveAspectRatio="xMidYMid slice"
-          />
-        </svg>
-        <h3 class="svgSted">Esbjerg Bowling Center</h3>
-        <p>D. 14/5 - 2025 kl 17:30 - 100DKK</p>
-        <h2>Bowling med Tidslerne</h2>
-        <button>Tilmeld Dig</button>
-      </div>
-      
-      <div style="width: calc(100%-60px); height: 500px; position: relative;" class="svgBowling">
-        <svg
-          viewBox="0 0 1837 500"
-          width="calc(100%-60px)"
-          height="100%"
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style="display: block; width: 100%; "
-        >
-          <mask id="clip-mask" maskUnits="objectBoundingBox">
-            <path fill="white" d="M0 50C0 22.3858 22.3858 0 50 0H1787C1814.61 0 1837 22.3858 1837 50V129.5C1837 157.114 1814.61 179.5 1787 179.5H1507.25C1482.81 179.5 1463 199.311 1463 223.75C1463 248.189 1443.19 268 1418.75 268H1078.5C1050.89 268 1028.5 290.386 1028.5 318V450C1028.5 477.614 1006.11 500 978.5 500H50C22.3858 500 0 477.614 0 450V50Z"/>
-          </mask>
-      
-          <image class="shapeImage"
-            href="images/bowling2.png"
-            width="1837"
-            height="500"
-            mask="url(#clip-mask)"
-            preserveAspectRatio="xMidYMid slice"
-          />
-        </svg>
-        <h3 class="svgSted">Esbjerg Bowling Center</h3>
-        <p>D. 14/5 - 2025 kl 17:30 - 100DKK</p>
-        <h2>Bowling med Tidslerne</h2>
-        <button>Tilmeld Dig</button>
-      </div>
+
+      <?php
+$arguments = array(
+  "post_type"=> "event-cards",
+  "posts_per_page" => 5, 
+);
+$loop = new WP_Query($arguments);
+?>
+
+<?php if($loop->have_posts()):?>
+  <?php while($loop->have_posts()): $loop->the_post()?> 
 
       <div style="width: calc(100%-60px); height: 500px; position: relative;" class="svgBowling">
         <svg
@@ -118,33 +76,10 @@
         <h2>Bowling med Tidslerne</h2>
         <button>Tilmeld Dig</button>
       </div>
-
-      <div style="width: calc(100%-60px); height: 500px; position: relative;" class="svgBowling">
-        <svg
-          viewBox="0 0 1837 500"
-          width="calc(100%-60px)"
-          height="100%"
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style="display: block; width: 100%; "
-        >
-          <mask id="clip-mask" maskUnits="objectBoundingBox">
-            <path fill="white" d="M0 50C0 22.3858 22.3858 0 50 0H1787C1814.61 0 1837 22.3858 1837 50V129.5C1837 157.114 1814.61 179.5 1787 179.5H1507.25C1482.81 179.5 1463 199.311 1463 223.75C1463 248.189 1443.19 268 1418.75 268H1078.5C1050.89 268 1028.5 290.386 1028.5 318V450C1028.5 477.614 1006.11 500 978.5 500H50C22.3858 500 0 477.614 0 450V50Z"/>
-          </mask>
       
-          <image class="shapeImage"
-            href="images/bowling2.png"
-            width="1837"
-            height="500"
-            mask="url(#clip-mask)"
-            preserveAspectRatio="xMidYMid slice"
-          />
-        </svg>
-        <h3 class="svgSted">Esbjerg Bowling Center</h3>
-        <p>D. 14/5 - 2025 kl 17:30 - 100DKK</p>
-        <h2>Bowling med Tidslerne</h2>
-        <button>Tilmeld Dig</button>
-      </div>
+     
+
+   
     </section>
       <!-- 2. Your masked section -->
       <div class="events"></div>
