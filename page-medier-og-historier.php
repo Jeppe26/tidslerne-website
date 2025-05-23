510a248960ed441ff3e159ea3e525f356032184e
AndersJ2812 <andjoe02@easv365.dk>
2025-05-23 13:08:39 +0200

    video

--- page-medier-og-historier.php
+++ page-medier-og-historier.php
@@ -97,12 +97,15 @@ $Billede11 = get_field("billede_11");
 </div>
 <button class="billedButton">Se mere</button>
 </section>
+<?php
+$Video1 = get_field("video_1");
+?>
 
 <section class="videoGaleri">
     <h2 class="billedTitlel">Video Galeri</h2>
     <div class="videoer">
         <div class="grid-container-video">
-            <div class="box"></div>
+            <div class="box"><iframe width="560" height="315" src="<?php echo esc_url(get_field('video_1')); ?>" alt=""></iframe></div>
             <div class="box"></div>
             <div class="box"></div>
             <div class="box"></div>
