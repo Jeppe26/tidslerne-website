e027ee034f334df03b02924aad01a0f6f0cfe3ac
AndersJ2812 <andjoe02@easv365.dk>
2025-05-23 13:00:24 +0200

    Medie billeder

--- page-medier-og-historier.php
+++ page-medier-og-historier.php
@@ -66,20 +66,31 @@ $ArtikelCardDatoInfo = get_field("artikel_card_dato_info");
 
       <?php
 $Billede1 = get_field("billede_1");
+$Billede2 = get_field("billede_2");
+$Billede3 = get_field("billede_3");
+$Billede4 = get_field("billede_4");
+$Billede5 = get_field("billede_5");
+$Billede6 = get_field("billede_6");
+$Billede7 = get_field("billede_7");
+$Billede8 = get_field("billede_8");
+$Billede9 = get_field("billede_9");
+$Billede10 = get_field("billede_10");
+$Billede11 = get_field("billede_11");
+
 
     ?>
       <style>
         .a{background-image: url("<?php echo esc_url($Billede1["url"]) ?>"); }
-        .b{background-image: url("images/oplægTids.jpg");}
-        .c{background-image: url("images/oplægTids.jpg");}
-        .d{background-image: url("images/oplægTids.jpg");}
-        .e{background-image: url("images/oplægTids.jpg");}
-        .f{background-image: url("images/oplægTids.jpg");}
-        .g{ background-image: url("images/oplægTids.jpg");}
-        .h{background-image: url("images/oplægTids.jpg");}
-        .i{background-image: url("images/oplægTids.jpg");}
-        .j{background-image: url("images/oplægTids.jpg");}
-        .k{background-image: url("images/oplægTids.jpg");}
+        .b{background-image: url("<?php echo esc_url($Billede2["url"]) ?>");}
+        .c{background-image: url("<?php echo esc_url($Billede3["url"]) ?>");}
+        .d{background-image: url("<?php echo esc_url($Billede4["url"]) ?>");}
+        .e{background-image: url("<?php echo esc_url($Billede5["url"]) ?>");}
+        .f{background-image: url("<?php echo esc_url($Billede6["url"]) ?>");}
+        .g{ background-image: url("<?php echo esc_url($Billede7["url"]) ?>");}
+        .h{background-image: url("<?php echo esc_url($Billede8["url"]) ?>");}
+        .i{background-image: url("<?php echo esc_url($Billede9["url"]) ?>");}
+        .j{background-image: url("<?php echo esc_url($Billede10["url"]) ?>");}
+        .k{background-image: url("<?php echo esc_url($Billede11["url"]) ?>");}
 
         
       </style>
