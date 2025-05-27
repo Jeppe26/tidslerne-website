46770119351afe4251657e1c0ad2af2becae04e9
benjasteck <benj5102@easv365.dk>
2025-05-27 10:35:38 +0200

    Update page-artikel.php

--- page-artikel.php
+++ page-artikel.php
@@ -63,6 +63,7 @@ $ArtikelCardDatoInfo = get_field("artikel_card_dato_info");
             <h2><?php echo $ArtikelCardTitel?></h2>
             <p><?php echo $ArtikelCardTekst?></p>
             <button><a href="https://tidslerne.nemtilmeld.dk/">Se mere</a></button>
+            
         </div>
     </div>
   </div>
